7ad52994c9206ebb0e50485bfeca795a54bae73d
Ruben <ruben-f@outlook.com>
2025-09-17 17:05:06 +0200

    0.6.2
    Starting to make a standard vga library. Added colours, char buffer, etc.

--- src/wldvga.rs
+++ src/wldvga.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 // 16 Bit colors.
@@ -18,4 +19,29 @@ pub enum Color {
     Pink = 13,
     Yellow = 14,
     White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
