326786fc2f60153fad0ae1e659e1c3f9ed7ae9b2
Ruben <ruben-f@outlook.com>
2025-09-17 18:33:51 +0200

    0.6.3
    Added write_string.

--- src/wldvga.rs
+++ src/wldvga.rs
@@ -45,3 +45,46 @@ const BUFFER_WIDTH: usize = 80;
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => Writer::new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    Writer::new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code: (color_code),
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // In ascii range.
+                0x20..0x7e | b'\n' => self.write_byte(byte),
+                //Not in ascii range.
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn new_line() { /* TODO! */
+    }
+}
