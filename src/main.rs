a98eff4d3525eb2a1e907e1575e9f56f7eb39645
Ruben <ruben-f@outlook.com>
2025-09-20 09:46:23 +0200

    0.7.6
    Started to add testing funcs.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod wldvga;
 
@@ -12,6 +15,14 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests.", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     wldvga::WRITER.lock().vga_paint();
@@ -22,7 +33,12 @@ pub extern "C" fn _start() -> ! {
     wldvga::WRITER
         .lock()
         .put_char(b'+', wldvga::BUFFER_WIDTH / 2, wldvga::BUFFER_HEIGHT / 2);
-    println!("Testing put_char cursor return. This sentence aught to be on row 2.");
+    println!(
+        "Testing put_char cursor return. This sentence aught to be on row 2 spanning all the way to 3 (Rows start at 0)."
+    );
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
