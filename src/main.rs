8ae99b953d958196a22689bb8cd0bfcbfeb72bdf
Ruben <ruben-f@outlook.com>
2025-09-21 14:10:31 +0200

    0.8.0
    Qemu now exits automatically when running a test.

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,12 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests.", tests.len());
+
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -49,4 +52,21 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
+}
+
+// Qemu Port setup.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
